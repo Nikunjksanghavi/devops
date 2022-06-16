726ca5cda2a9db171b2949a53ba8fa21276c564c
Nikunj Sanghavi <nksanghavi1993@gmail.com>
2022-06-16 16:18:18 +0530

    maven project

---
+++ src/main/java/com/devops_nik/Arithmetic.java
@@ -0,0 +1,11 @@
+package com.devops_nik;
+
+public class Arithmetic {
+	public int product(int number1, int number2) {
+		return number1 * number2;
+	}
+
+	public int division(int number1, int number2) {
+		return number1 / number2;
+	}
+}
---
+++ src/main/java/com/devops_nik/Try.java
@@ -0,0 +1,8 @@
+package com.devops_nik;
+
+public class Try {
+
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
+}
